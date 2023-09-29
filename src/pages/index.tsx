5b02c3c68d88aca818ab564804c750c2cc410051
soezyxstt <13122080@mahasiswa.itb.ac.id>
2023-09-29 08:16:31 +0700

    add landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,467 @@
-import { Flex, Text } from "@chakra-ui/react";
-import styles from "./index.module.css";
-import { signIn, signOut, useSession } from "next-auth/react";
-import Head from "next/head";
-import Link from "next/link";
-import { PublicLayout } from "~/components/layout/PublicLayout";
-import { withSession } from "~/server/auth/withSession";
-import { api } from "~/utils/api";
+import {
+  Button,
+  Flex,
+  FlexProps,
+  Grid,
+  Heading,
+  Image,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
+import styles from './index.module.css';
+import { signIn, signOut, useSession } from 'next-auth/react';
+import Head from 'next/head';
+import Link from 'next/link';
+import { PublicLayout } from '~/components/layout/PublicLayout';
+import { withSession } from '~/server/auth/withSession';
+import { api } from '~/utils/api';
 
 export default function Home() {
   return (
     <>
       <PublicLayout>
-        <Flex flexDir="column" alignItems="center">
-          <Text fontSize="3xl" fontWeight="bold" textAlign="center" mt="4rem">
-            IECOM 2024
-          </Text>
+        {/* First Section */}
+        <Flex
+          paddingBlock={paddings.xxl}
+          paddingInline={paddings.xxxl}
+          mt='4rem'
+          gap={{ base: '2rem', md: '4rem' }}
+          alignItems='center'
+        >
+          <Image
+            alt='IECOM'
+            src='main-icon.webp'
+            h={{ base: '6rem', md: '18rem' }}
+          />
+          <VStack
+            align='flex-start'
+            color='blue'
+            h='100%'
+            justifyContent='center'
+          >
+            <Heading
+              fontFamily='body'
+              fontSize={{ base: fontSizes.xl.base, md: fontSizes.xxxl.md }}
+            >
+              Industrial Engineering Competition
+            </Heading>
+            <Text
+              fontSize={fontSizes.base}
+              fontFamily='Inter'
+              display={{ base: 'none', md: 'block' }}
+            >
+              Lorem ipsum dolor sit amet consectetur. Accumsan blandit ac diam
+              sed.
+            </Text>
+            <Button
+              fontSize={fontSizes.base}
+              size={{ base: 'sm', md: 'lg' }}
+            >
+              Learn More
+            </Button>
+          </VStack>
         </Flex>
+
+        {/* Second Section */}
+        <Flex
+          paddingBlock={paddings.base}
+          pl={paddings.xxxl}
+          pr={paddings.lg}
+          direction={{ base: 'column', md: 'row' }}
+          gap='2rem'
+          bgColor='brown.3'
+          justifyContent='space-between'
+        >
+          <Text
+            fontSize={fontSizes.base}
+            fontFamily='Inter'
+            width={{ base: 'auto', md: '65ch' }}
+          >
+            {COMP_DESCRIPTION}
+          </Text>
+          <Flex gap='1.5rem'>
+            <Image
+              alt='logo-mti'
+              src='landing-page/logo-mti.webp'
+              width={{ base: '5rem', md: '10.5rem' }}
+              aspectRatio={1}
+            />
+            <Image
+              alt='logo-itb.webp'
+              src='landing-page/logo-itb.webp'
+              width={{ base: '5rem', md: '10.5rem' }}
+              aspectRatio={1}
+            />
+          </Flex>
+        </Flex>
+
+        {/* Third Section */}
+        <VStack
+          padding={paddings.xl}
+          width='100%'
+          spacing={{ base: '3rem', md: '5rem' }}
+        >
+          <VStack
+            color='blue'
+            width='100%'
+            spacing={{ base: '1.5rem', md: '2.5rem' }}
+          >
+            <Heading
+              fontSize={fontSizes.xxl}
+              fontFamily='body'
+            >
+              OUR EVENTS
+            </Heading>
+            <EventCard
+              title='Pre Event'
+              content={PRE_EVENT_DESCRIPTION}
+              img='landing-page/pre-event.webp'
+            />
+            <EventCard
+              title='Essay Competition'
+              content={ESSAY_COMPETITION_DESCRIPTION}
+              img='landing-page/essay-competition.webp'
+            />
+            <EventCard
+              title='Grand Summit'
+              content={GRAND_SUMMIT_DESCRIPTION}
+              img='landing-page/grand-summit.webp'
+            />
+          </VStack>
+          <VStack
+            color='blue'
+            spacing={{ base: '2rem', md: '3rem' }}
+            width='100%'
+            mb={{ base: '2rem', md: '4rem' }}
+          >
+            <Heading
+              fontSize={fontSizes.xxl}
+              fontFamily='body'
+            >
+              PHASE & TIMELINE
+            </Heading>
+            <Flex
+              position='relative'
+              borderRight={{ base: 'none', md: 'solid #054E83' }}
+              borderLeft={{ md: 'none', base: 'solid #054E83' }}
+              borderBlock={{ base: 'none', md: 'solid #054E83' }}
+              borderWidth={{ base: '4px', md: '.625rem' }}
+              width='80%'
+              height={{ base: '35rem', md: '12.5rem' }}
+            >
+              {Array(8)
+                .fill(0)
+                .map((_, index) => (
+                  <TimelineCard
+                    key={index}
+                    title={`Phase ${index + 1}`}
+                    date='20 September 2022'
+                    top={`calc(${index * 5}rem - 1rem)`}
+                    left={`calc(${
+                      (index - 4 * Math.floor(index / 4)) * 18
+                    }rem - 8rem)`}
+                    isLower={index > 3}
+                  />
+                ))}
+            </Flex>
+          </VStack>
+          <VStack>
+            <Heading
+              fontSize={fontSizes.xxl}
+              fontFamily='body'
+              textAlign='center'
+              color='blue'
+              mb={{ base: '2rem', md: '4rem' }}
+            >
+              WHAT THEY SAY ABOUT IECOM
+            </Heading>
+            <Grid
+              gap='1rem'
+              gridTemplateColumns={{
+                base: 'repeat(1, 1fr)',
+                md: 'repeat(3, 1fr)',
+              }}
+            >
+              {Testimonial.map((item, index) => (
+                <TestimonialCard
+                  key={index}
+                  content={item.content}
+                  img={'landing-page/logo-itb.webp'}
+                  author={item.author}
+                  authorPos={item.authorPos}
+                />
+              ))}
+            </Grid>
+          </VStack>
+        </VStack>
       </PublicLayout>
     </>
   );
 }
+
+const COMP_DESCRIPTION =
+  'IECOM is an international industrial engineering competition designed for undergraduate students majoring in Industrial Engineering, Engineering Management, or other related fields, held by Industrial Engineering Student Union of Bandung Institute of Technology (MTI ITB).';
+
+const PRE_EVENT_DESCRIPTION =
+  "IECOM 2022 will also give you a series of events to explore more about Industrial Engineering, especially to be the answer to today's problem. Grab the chance to learn from best expertise who’ll share their knowledge and experience about Sustainable Industry";
+
+const ESSAY_COMPETITION_DESCRIPTION =
+  'The essay competition provides a place to be creative and solution-oriented to answer a problem related to the main topic “Refining Southeast Asia’s Business through Entrepreneurship in the Disruptive Era”';
+
+const GRAND_SUMMIT_DESCRIPTION =
+  "The Grand Summit is a national seminar that will discuss current issues related to IECOM 2022's grand theme “Industry development through digitalization and innovation”.";
+
+const Testimonial = [
+  {
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, voluptatum.',
+    img: 'landing-page/testimonial-1.webp',
+    author: 'John Doe',
+    authorPos: 'CEO of Lorem Ipsum',
+  },
+  {
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, voluptatum.',
+    img: 'landing-page/testimonial-2.webp',
+    author: 'Jonathan Doe',
+    authorPos: 'CEO of Lorem Ipsum',
+  },
+  {
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, voluptatum.',
+    img: 'landing-page/testimonial-3.webp',
+    author: 'Michael Doe',
+    authorPos: 'CEO of Lorem Ipsum',
+  },
+];
+
+const EventCard = ({
+  title,
+  content,
+  img,
+}: {
+  title: string;
+  content: string;
+  img: string;
+}) => {
+  return (
+    <Flex
+      direction={{ base: 'column', md: 'row' }}
+      alignItems='center'
+      bgColor='brown.5'
+      padding='1rem 1.5rem 1rem 3rem'
+      width='100%'
+      gap={{ base: '1.25rem', md: '2rem' }}
+    >
+      <VStack align='flex-start'>
+        <Heading
+          fontSize={fontSizes.lg}
+          fontFamily='body'
+        >
+          {title}
+        </Heading>
+        <Text
+          fontSize={fontSizes.base}
+          fontWeight='semibold'
+        >
+          {content}
+        </Text>
+      </VStack>
+      <Image
+        alt='image'
+        src={img}
+        aspectRatio={16 / 9}
+        height={{ base: 'auto', md: '16rem' }}
+      />
+    </Flex>
+  );
+};
+
+const TimelineCard = ({
+  title,
+  date,
+  top,
+  left,
+  isLower = false,
+  ...props
+}: FlexProps & { title: string; date: string; isLower?: boolean }) => {
+  const topPos = !isLower
+    ? 'calc(-1.75rem - .625rem/2)'
+    : 'calc(12.5rem - 1.75rem - .625rem * 3/2)';
+  return (
+    <Flex
+      direction={{ base: 'row', md: 'column' }}
+      position='absolute'
+      top={{
+        base: top as string,
+        md: topPos,
+      }}
+      left={{ md: left as string, base: 'calc(-1rem - 2px)' }}
+      {...props}
+      width='fit-content'
+      alignItems='center'
+      gap={{ base: '1rem', md: '0' }}
+    >
+      <Flex
+        bgColor='white'
+        borderRadius='50%'
+        height={{ base: '2rem', md: '3.5rem' }}
+        width={{ base: '2rem', md: '3.5rem' }}
+      />
+      <VStack
+        spacing={0}
+        align={{ base: 'flex-start', md: 'center' }}
+      >
+        <Text
+          fontSize={fontSizes.md}
+          fontWeight='semibold'
+          color='blue'
+          fontFamily='body'
+        >
+          {date}
+        </Text>
+        <Text
+          fontSize={fontSizes.base}
+          color='blue'
+          fontFamily='Inter'
+        >
+          {title}
+        </Text>
+      </VStack>
+    </Flex>
+  );
+};
+
+const TestimonialCard = ({
+  content,
+  img,
+  author,
+  authorPos,
+}: {
+  content: string;
+  img: string;
+  author: string;
+  authorPos: string;
+}) => {
+  return (
+    <Flex
+      direction='column'
+      color='rgba(0, 0, 0, .71)'
+      width='100%'
+      fontFamily='Inter'
+    >
+      <Flex
+        padding={paddings.sm}
+        bgColor='brown.5'
+      >
+        <Text
+          fontWeight='semibold'
+          fontSize={fontSizes.sm}
+          textAlign='center'
+          lineHeight='95%'
+        >
+          {content}
+        </Text>
+      </Flex>
+      <Flex
+        gap='1rem'
+        alignItems='center'
+        padding='1rem'
+      >
+        <Image
+          alt='image'
+          src={img}
+          borderRadius='50%'
+          aspectRatio={1}
+          height={{ base: '3.5rem', md: '5rem' }}
+          objectFit='cover'
+          objectPosition='center'
+        />
+        <VStack
+          spacing={0}
+          fontSize={fontSizes.xs}
+          align='flex-start'
+          lineHeight='100%'
+        >
+          <Text fontWeight='bold'>{author}</Text>
+          <Text
+            fontWeight='semibold'
+            color='rgba(0, 0, 0, .5)'
+          >
+            {authorPos}
+          </Text>
+        </VStack>
+      </Flex>
+    </Flex>
+  );
+};
+
+const fontSizes = {
+  xs: {
+    base: '0.75rem',
+    md: '1rem',
+  },
+  sm: {
+    base: '0.875rem',
+    md: '1.25rem',
+  },
+  base: {
+    base: '1rem',
+    md: '1.5rem',
+  },
+  md: {
+    base: '1.15rem',
+    md: '2rem',
+  },
+  lg: {
+    base: '1.25rem',
+    md: '2.5rem',
+  },
+  xl: {
+    base: '1.5rem',
+    md: '3rem',
+  },
+  xxl: {
+    base: '1.75rem',
+    md: '3.5rem',
+  },
+  xxxl: {
+    base: '2rem',
+    md: '4rem',
+  },
+};
+
+const paddings = {
+  xs: {
+    base: '.75rem',
+    md: '1rem',
+  },
+  sm: {
+    base: '1rem',
+    md: '2rem',
+  },
+  base: {
+    base: '1.25rem',
+    md: '3.5rem',
+  },
+  md: {
+    base: '1.5rem',
+    md: '4rem',
+  },
+  lg: {
+    base: '1.75rem',
+    md: '5rem',
+  },
+  xl: {
+    base: '2rem',
+    md: '6rem',
+  },
+  xxl: {
+    base: '2.25rem',
+    md: '7rem',
+  },
+  xxxl: {
+    base: '2.5rem',
+    md: '9rem',
+  },
+};
--- src/styles/component/colors.tsx
+++ src/styles/component/colors.tsx
@@ -8,13 +8,17 @@ const black = '#341F16';
 const cream = '#F0E1CC';
 const whiteCream = '#F9F6F0';
 
+const brown = {
+  3: '#C3A580',
+  5: '#FFF7E7',
+};
 
 const gray = {
   200: '#F9F9F9',
   300: '#BFBFBF',
   400: '#9B9B9B',
   500: '#777676',
-  600: '#2F2E2E'
+  600: '#2F2E2E',
 };
 
 const navy = {
@@ -22,7 +26,7 @@ const navy = {
   2: '#2B0792',
   3: '#3608C0',
   4: '#3F2CEB',
-  5: '#4A58F6'
+  5: '#4A58F6',
 };
 
 const pink = {
@@ -30,7 +34,7 @@ const pink = {
   2: '#E935B8',
   3: '#F74DC1',
   4: '#FF67C3',
-  5: '#FF93D1'
+  5: '#FF93D1',
 };
 
 const yellow = {
@@ -38,7 +42,7 @@ const yellow = {
   2: '#F79A2E',
   3: '#FFBE3B',
   4: '#FFE655',
-  5: '#FFFC83'
+  5: '#FFFC83',
 };
 
 const green = {
@@ -46,7 +50,7 @@ const green = {
   2: '#1C939A',
   3: '#2FC1AD',
   4: '#72D8BA',
-  5: '#C0EACA'
+  5: '#C0EACA',
 };
 
 const purple = {
@@ -54,7 +58,7 @@ const purple = {
   2: '#4909B3',
   3: '#651CCE',
   4: '#7B34E1',
-  5: '#8D47E5'
+  5: '#8D47E5',
 };
 
 export const colors = {
@@ -69,5 +73,6 @@ export const colors = {
   purple,
   blue,
   cream,
-  whiteCream
+  brown,
+  whiteCream,
 };
